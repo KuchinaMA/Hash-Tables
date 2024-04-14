2e7cdbdcbb4dc784b57b5130842214c21429c9e2
KuchinaMA <m.kuchina05@mail.ru>
2024-04-14 21:34:38 +0300

    Optimization

--- HashTables.cpp
+++ HashTables.cpp
@@ -1,10 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <strings.h>
+#include <cstring>
 #include <assert.h>
 
 #include "HashTables.h"
 #include "HashFuncs.h"
 
+extern "C" int find_key_asm(HashTable * table, uint32_t hash_val, const char* value);
 
 Node* node_ctor(const char* value) {
 
@@ -21,7 +23,7 @@ Node* node_ctor(const char* value) {
 
 void node_dtor(Node* node) {
 
-    node->value = "";
+    //node->value = "";
     node->next = NULL;
     node->prev = NULL;
 
@@ -83,13 +85,14 @@ void hashtable_dtor(HashTable* table) {
 }
 
 
-void hashtable_add(HashTable* table, const char* value, uint32_t (*hash_func) (const char* str)) {
+void hashtable_add(HashTable* table, const char* value, uint32_t(*hash_func) (const char* str)) {
 
     Node* new_node = node_ctor(value);
 
     uint32_t hash_val = hash_func(value) % table->cells_num;
 
-    if (hashtable_find_key(table, hash_val, value) == NOT_FOUND) {
+    //if (hashtable_find_key(table, hash_val, value) == NOT_FOUND) {
+    if (find_key_asm(table, hash_val, value) == NOT_FOUND) {
 
         if (table->cells[hash_val].size == 0) {
 
@@ -118,7 +121,8 @@ int hashtable_find_key(HashTable* table, uint32_t hash_val, const char* value) {
 
     while (key_cell != NULL) {
 
-        if (strcmp(value, key_cell->value) == 0)
+        //if (strcmp(value, key_cell->value) == 0)
+        if (strcmp_asm(value, key_cell->value) == 0)
             return FOUND;
 
         key_cell = key_cell->next;
@@ -129,11 +133,11 @@ int hashtable_find_key(HashTable* table, uint32_t hash_val, const char* value) {
 
 }
 
-void get_list_sizes(size_t htabe_capacity, TextData text, const char* output_name, uint32_t (*hash_func) (const char* str)) {
+void get_list_sizes(size_t htable_capacity, TextData text, const char* output_name, uint32_t(*hash_func) (const char* str)) {
 
     FILE* output_file = fopen(output_name, "w");
 
-    HashTable* table = hashtable_ctor(htabe_capacity);
+    HashTable* table = hashtable_ctor(htable_capacity);
 
     for (int i = 0; i < text.nLines; ++i) {
         hashtable_add(table, (text.text[i]).pointer, hash_func);
@@ -151,136 +155,49 @@ void get_list_sizes(size_t htabe_capacity, TextData text, const char* output_nam
 
 }
 
+void insert_all_data(HashTable* table, TextData text, uint32_t(*hash_func) (const char* str)) {
 
-
-
-
-
-
-/*uint32_t hash_1(const char* str) {
-
-    return 1;
-
-}
-
-uint32_t hash_2(const char* str) {
-
-    return (uint32_t)(str[0] - 'a');
-
-}
-
-uint32_t hash_3(const char* str) {
-
-    int str_length = strlen(str);
-    uint32_t hash_sum = 0;
-
-    for (int i = 0; i < str_length; i++) {
-        hash_sum += (uint32_t)str[i];
-    }
-    //printf("%u\n", hash_sum);
-    return hash_sum;
-}
-
-uint32_t hash_4(const char* str) {
-
-    //printf("%s %d", str, strlen(str));
-    return (uint32_t)strlen(str);
-
-}
-
-uint32_t hash_5(const char* str) {
-
-    uint32_t hash_sum = 0;
-
-    size_t len = strlen(str);
-
-    for (size_t i = 0; i < len; i++) {
-
-        hash_sum = rol(hash_sum) ^ str[i];                 //в ридми привести текст этой функции
-                                                           //+ как компилирует gcc без оптимизаций, с -O2 и -O3
-    }
-    //printf("%u\n", hash_sum);
-    return hash_sum;
-
-}
-
-uint32_t hash_6(const char* str) {
-
-    uint32_t hash_sum = 0;
-
-    size_t len = strlen(str);
-    printf("%d\n", len);
-
-    for (size_t i = 0; i < len; i++) {
-
-        hash_sum = ror(hash_sum) ^ str[i];
-        printf("%u\n", hash_sum);
+    for (int i = 0; i < text.nLines; ++i) {
+        hashtable_add(table, (text.text[i]).pointer, hash_func);
     }
 
-    printf("\n%u\n", hash_sum);
-    return hash_sum;
+    printf("done\n");
 
-}  */
+}
 
-/*uint32_t hash_7(const char* str) {
+void find_words(HashTable* table, TextData text, uint32_t(*hash_func) (const char* str)) {
 
-    const uint32_t crc_table[256] = {0x00000000, 0x04C11DB7, 0x09823B6E, 0x0D4326D9,
-        // остальные значения таблицы CRC32
-    };
+    for (size_t i = 0; i < FIND_WORDS_NUM; i++) {
 
-    uint32_t hash_sum = 0xFFFFFFFF;
-    size_t len = strlen(str);
+        for (int i = 0; i < text.nLines; i++) {
 
-    for (size_t i = 0; i < len; i++) {
-
-        hash_sum = hash_sum ^ ((uint32_t)str[i] << 24);
-
-        for (int j = 0; j < 8; j++) {
-            //hash_sum = (hash_sum << 1) ^ (crc_table[hash_sum >> 24]);
-            if (hash_sum & (1L << 31))
-                hash_sum = (hash_sum << 1) ^ 0x04C11DB7;
-            else
-                hash_sum = (hash_sum << 1);
+            const char* key = (text.text[i]).pointer;
+            uint32_t hash_val = hash_func(key) % table->cells_num;
+            hashtable_find_key(table, hash_val, key);
         }
     }
-
-    return hash_sum;
-
-} */
-
-/*uint32_t hash_7(const char* str) {
-
-    uint32_t crc_table[256];
-	uint32_t crc = 0;
-	size_t len = strlen(str);
-
-	for (int i = 0; i < 256; i++) {
-		crc = i;
-
-		for (int j = 0; j < 8; j++)
-			crc = crc & 1 ? (crc >> 1) ^ 0xEDB88320U : crc >> 1;
-
-		crc_table[i] = crc;
-	}
-
-	crc = 0xFFFFFFFFU;
-
-	while (len--)
-        crc = crc_table[(crc ^ *str++) & 0xFF] ^ (crc >> 8);
-
-	printf("%u\n", crc ^ 0xFFFFFFFFU);
-	return crc ^ 0xFFFFFFFFU;
 }
 
+inline int strcmp_asm(const char* str1, const char* str2) {
 
-uint32_t rol(uint32_t value) {
+    __asm {
+        push esi
+        push edi
 
-    return ((value << 1)) | (value >> 31);
+        mov esi, str1
+        mov edi, str2
 
+        xor eax, eax
+
+        vmovdqu ymm0, YMMWORD PTR [esi]
+        vmovdqu ymm1, YMMWORD PTR [edi]
+
+        vptest     ymm0, ymm1
+        setnc      eax
+
+        pop edi
+        pop esi
+
+    }
 }
 
-uint32_t ror(uint32_t value) {
-
-    return ((value >> 1)) | (value << 31);
-
-} */
