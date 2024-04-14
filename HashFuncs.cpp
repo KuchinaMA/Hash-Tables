b69b4280c69d556db554929012e2fb05d8707f0a
KuchinaMA <m.kuchina05@mail.ru>
2024-04-14 21:33:05 +0300

    Optimization

--- HashFuncs.cpp
+++ HashFuncs.cpp
@@ -1,6 +1,8 @@
+
 #include <stdio.h>
-#include <strings.h>
+#include <cstring>
 #include <assert.h>
+#include <immintrin.h>
 
 #include "HashFuncs.h"
 
@@ -76,54 +78,37 @@ inline uint32_t ror(uint32_t value) {
 
 }
 
-/*uint32_t hash_7(const char* str) {
-
-    const uint32_t crc_table[256] = {0x00000000, 0x04C11DB7, 0x09823B6E, 0x0D4326D9,
-        // остальные значения таблицы CRC32
-    };
-
-    uint32_t hash_sum = 0xFFFFFFFF;
-    size_t len = strlen(str);
-
-    for (size_t i = 0; i < len; i++) {
-
-        hash_sum = hash_sum ^ ((uint32_t)str[i] << 24);
-
-        for (int j = 0; j < 8; j++) {
-            //hash_sum = (hash_sum << 1) ^ (crc_table[hash_sum >> 24]);
-            if (hash_sum & (1L << 31))
-                hash_sum = (hash_sum << 1) ^ 0x04C11DB7;
-            else
-                hash_sum = (hash_sum << 1);
-        }
-    }
-
-    return hash_sum;
-
-} */
 
 uint32_t crc32_hash(const char* str) {
 
     uint32_t crc_table[256];
-	uint32_t crc = 0;
-	size_t len = strlen(str);
+    uint32_t crc = 0;
+    size_t len = strlen(str);
 
-	for (int i = 0; i < 256; i++) {
-		crc = i;
+    for (int i = 0; i < 256; i++) {
+        crc = i;
 
-		for (int j = 0; j < 8; j++)
-			crc = crc & 1 ? (crc >> 1) ^ 0xEDB88320U : crc >> 1;
+        for (int j = 0; j < 8; j++)
+            crc = crc & 1 ? (crc >> 1) ^ 0xEDB88320U : crc >> 1;
 
-		crc_table[i] = crc;
-	}
+        crc_table[i] = crc;
+    }
 
-	crc = 0xFFFFFFFFU;
+    crc = 0xFFFFFFFFU;
 
-	while (len--)
+    while (len--)
         crc = crc_table[(crc ^ *str++) & 0xFF] ^ (crc >> 8);
 
-	//printf("%u\n", crc ^ 0xFFFFFFFFU);
-	return crc ^ 0xFFFFFFFFU;
+    return crc ^ 0xFFFFFFFFU;
 }
 
+uint32_t crc32_hash_intrinsic(const char* str) {
 
+    uint32_t crc = 0;
+    crc = _mm_crc32_u32(crc, *((uint64_t*)str ));
+    crc = _mm_crc32_u32(crc, *((uint64_t*)str + 1));
+    crc = _mm_crc32_u32(crc, *((uint64_t*)str + 2));
+    crc = _mm_crc32_u32(crc, *((uint64_t*)str + 3));
+
+    return crc;
+}
